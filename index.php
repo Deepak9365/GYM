3103019f88811419ed0ffda7927c1a24c0349175
unknown <kumawat9365@gmail.com>
2023-10-30 04:50:07 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,361 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>The fit life gym</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="style.css">
+ 
+</head>
+
+<body>
+  
+
+  <nav class="navbar navbar-expand-lg mycolor ">
+    <div class="container-fluid">
+      <a class="navbar-brand text-capitalize text-white" href="#">The fitlife GYM</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-capitalize" href="#Aboutthepage">About Us</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link text-capitalize" href="#servicethepage">Services </a>
+
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-capitalize" href="#gallerythepage">gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-capitalize" href="#contactthepage">contact us</a>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+
+  <!-- header part start -->
+  <header>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+          aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="Gymimage/image1.jpg" class="d-block w-100" alt="Gymimage/image1.jpg">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="Gymimage/image2.jpg" class="d-block w-100" alt="Gymimage/image2.jpg">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="Gymimage/image3.jpg" class="d-block w-100" alt="Gymimage/image3.jpg">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </header>
+  <!-- header part end -->
+
+  <!-- position part -->
+  <!-- <section >
+  <div class="container ">
+    <div class="row">
+      <div class="col-md-8"></div>
+      <div class="col-md-4 gymname">
+        <h3 class="text-center">The fitlife GYM</h3>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, harum?</p>
+      </div>
+    </div>
+  </div>
+</section> -->
+
+  <!-- section about us -->
+
+
+  <section class="aboutus mt-5" id="Aboutthepage" >
+    <div class="hr">
+      <h2 class="text-center">About Us </h2>
+      <hr style="width:220px">
+    </div>
+
+
+    <div class="container mt-3">
+      <div class="row my-5">
+        <div class=" col-md-6  ">
+          <figure>
+            <img src="./Gymimage/image1.jpg" alt="error" class="img-fluid" style="border-radius: 29px;">
+          </figure>
+        </div>
+        <div class=" col-md-6 d-flex justify-content-around align-items-center flex-column">
+          <h1>Heading </h1>
+          <p>Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora saepe laudantium ex quis
+            eligendi earum corporis quo vel maxime totam! sit amet consectetur adipisicing elit. Hic quas, dignissimos
+            corrupti dolore officia obcaecati non porro laborum, repudiandae voluptates similique? Quis adipisci animi
+            tempora reprehenderit iure earum numquam voluptatem.
+
+          </p>
+          <a href="https://www.gymandfitness.com.au/pages/about-us" target="_blank">
+          <button class="btn btn-outline-info">Show More</button>
+        </a>
+        </div>
+      </div>
+    </div>
+
+    <!-- about Us end  -->
+
+    <!-- services section start -->
+
+  </section>
+
+  <section class="service mt-5 bg-light pt-3" id="servicethepage">
+    <div class="hr">
+      <h2 class="text-center">Services </h2>
+      <hr style="width:220px">
+    </div>
+    <div class="container mt-5 services">
+      <div class="row">
+        <div class="col-md-4 mx-auto">
+          <div class="card" style="max-width: 540px;">
+            <div class="row ">
+              <div class="col-md-4  ">
+                <img src="./Gymimage/photo1.jpg" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mx-auto">
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row ">
+              <div class="col-md-4">
+                <img src="./Gymimage/photo2.jpg" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mx-auto">
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row ">
+              <div class="col-md-4">
+                <img src="./Gymimage/photo3.jpg" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xxl-12 text-center">
+        <button class="btn btn-outline-info "><a href="https://klub20.ca/en/gym-services-and-features" class="text-decoration-none "
+            target="_blank">SHow more</a></button>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- services section end  -->
+
+  <!-- gallery start  -->
+  <section class="service mt-5 " id="gallerythepage"> 
+    <div class="hr">
+      <h2 class="text-center">Gallery </h2>
+      <hr style="width:220px">
+    </div>
+
+    <div class="container">
+      <div class="row gy-4 my-4">
+        <div class="col-md-4 col-10 col-xxl-4 mx-auto">
+          <figure>
+            <img src=" ./Gymimage/gallery1.webp " class="img-fluid" alt="gallery">
+          </figure>
+        </div>
+        <div class="col-md-4 col-10 col-xxl-4 mx-auto">
+          <figure>
+            <img src="./Gymimage/gallery1.webp   " class="img-fluid" alt="gallery">
+          </figure>
+        </div>
+        <div class="col-md-4 col-10 col-xxl-4 mx-auto">
+          <figure>
+            <img src=" ./Gymimage/gallery1.webp " class="img-fluid" alt="gallery">
+          </figure>
+        </div>
+        <div class="col-md-4 col-10 col-xxl-4 mx-auto">
+          <figure>
+            <img src=" ./Gymimage/gallery1.webp  " class="img-fluid" alt="gallery">
+          </figure>
+        </div>
+        <div class="col-md-4 col-10 col-xxl-4 mx-auto">
+          <figure>
+            <img src=" ./Gymimage/gallery1.webp  " class="img-fluid" alt="gallery">
+          </figure>
+        </div>
+        <div class="col-md-4 col-10 col-xxl-4 mx-auto">
+          <figure>
+            <img src="  ./Gymimage/gallery1.webp " class="img-fluid" alt="gallery">
+          </figure>
+        </div>
+        <div class="col-md-4 col-10 col-xxl-4 mx-auto">
+          <figure>
+            <img src="   ./Gymimage/gallery1.webp" class="img-fluid" alt="gallery">
+          </figure>
+        </div>
+        <div class="col-md-4 col-10 col-xxl-4 mx-auto">
+          <figure>
+            <img src="./Gymimage/gallery1.webp " class="img-fluid" alt="gallery">
+          </figure>
+        </div>
+        <div class="col-md-4 col-10 col-xxl-4 mx-auto">
+          <figure>
+            <img src=" ./Gymimage/gallery1.webp " class="img-fluid" alt="gallery">
+          </figure>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- gallery end  -->
+
+  <!-- galery extra  -->
+  <section class="my-5 pt-2 offer_style">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xxl-12 col-12 text-center pt-2">
+          <h1 class="text-white">check out my videos</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,mollitia.
+
+          </p>
+
+          <button class="btn btn-outline-light "><a href="https://www.youtube.com/watch?v=kf0Af6A5wW8"
+              class="text-decoration-none " target="_blank">SHow more</a></button>
+        </div> 
+
+      </div>
+    </div>
+  </section>
+  <!-- gallery extra end  -->
+
+  <!-- contact us start  -->
+  
+
+  <section class="service mt-5 bg-light " id="contactthepage">
+    <div class="hr">
+      <h2 class="text-center">Contact Us </h2>
+      <hr style="width:220px">
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xxl-8 col-10 col-md-8 pt-4 mx-auto">
+          <form action="data.php" method="post">
+
+            <div class="mb-3">
+              <label for="exampleCheck1" class="form-label">Name</label>
+              <input type="text" class="form-control" name="name" id="exampleInputcheck1">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputmobile" class="form-label">Mobile no.</label>
+              <input type="number" class="form-control"  name="contact" id="numberHelp" aria-describedby="numberHelp">
+              <div id="numberHelp" class="form-number">We'll never share your mobile no. with anyone else.</div>
+            </div>
+           
+            <button type="submit" name="buttonn" class="btn btn-primary">Submit</button>
+          </form>
+
+        </div>
+
+      </div>
+    </div>
+
+
+  </section>
+
+  <!-- contact us end  -->
+  <section class="mt-5 pt-2 offer_bottom">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xxl-12 col-12 text-center py-3">
+
+
+          <p class="text-white bg-dark">This is your social media pages links click and check them also.</p>
+          <a href="https://www.instagram.com/the_fitlife_gym_/" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a>
+          <a href="https://www.facebook.com/josemejia.gt/videos/2871295426532823" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a>
+          <a href="https://twitter.com/explore" target="_blank"><ion-icon name="logo-twitter"></ion-icon></a>
+          <a href="https://www.youtube.com/@thefitlifegym6276" target="_blank"><ion-icon name="logo-youtube"></ion-icon></a>
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <!-- icons links  -->
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <!-- icons links end  -->
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
+   
+</body>
+
+</html>
